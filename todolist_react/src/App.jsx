dab13c1145e4087db9cb5e2e649cc2c7b4df6f1e
9bonyo2 <2byku999999999@gmail.com>
2026-09-21 09:38:35 +0900

    student zustand 전까지

---
+++ todolist_react/src/App.jsx
@@ -0,0 +1,13 @@
+import AppTodo from './AppTodo';
+import UseCallbackApp from './components/UseCallbackApp';
+
+const App = () => {
+    return (
+        <div>
+            {/* <AppTodo /> */}
+            <UseCallbackApp />
+        </div>
+    );
+};
+
+export default App;
